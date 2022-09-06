14c0d65d84587467fcb79dec4d8eb0a0619fe0c4
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-09-06 09:50:30 -0300

    Clase 04 - Renderizado condicional

--- Clase_04/app.js
+++ Clase_04/app.js
@@ -1,3 +1,22 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        email: '',
+        pass: '',
+        error: false
+    },
+    methods: {
+        login: function(){
+            console.info('Login...');
+            if( this.email == 'admin@gmail.com' && this.pass == '123'){
+                alert('Bienvenido');
+                this.error = false;
+            } else {
+                this.error = true;
+            }
+        },
+        ocultarError: function(){
+            this.error = false;
+        }
+    }
 })
