8feeff963f99ba04a4bff6d0524e2890871d6390
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2022-10-18 10:50:59 -0300

    Clase 10 - Componentes ComunicaciÃón

--- Clase_10/components/task.js
+++ Clase_10/components/task.js
@@ -5,9 +5,9 @@ Vue.component('task',{
     },
     template: // html
             `<li>
-                <span v-text="nombre"></span>
-                <strong v-if="estado">  Completa  </strong>
-                <strong v-else>  Pendiente  </strong>
+                <strong v-text="nombre" v-bind:class="{'text-success': estado, 'text-danger': !estado }"></strong>
+                <span v-if="estado">  Completa  </span>
+                <span v-else>  Pendiente  </span>
                 <button v-on:click="$emit('cambio')" class="btn btn-success btn-sm"><i class="fa-solid fa-check"></i></button>
             </li>`,
     methods:{
